de8ca30182cef0fbaf834f7f888dfbf3fe9a537e
Bobomurod Amirov <bobomurodamirov.b@gmail.com>
2024-11-05 10:07:28 +0500

    :rocket:

--- index.php
+++ index.php
@@ -1,37 +1,59 @@
+<?php
+$conn = new PDO("mysql:host=localhost;dbname=work_of_tracker", 'root', 'My$par0l');
+
+$query = "SELECT * FROM work_time ORDER BY id DESC";
+
+$stmt = $conn->query($query);
+
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+const WORK_TIME = 8;
+
+?>
+
 <!doctype html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Work of tracker</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
-        body{
+        body {
             background-color: black;
         }
 
-        .row-width{
+        .row-width {
             width: 50%;
             margin-left: 270px;
             background-color: gold;
             border-radius: 20px;
         }
 
-        .container{
+        .container {
             text-align: center;
         }
 
-        .color{
+        .color {
             color: red;
+        }
+
+        .h1-color {
+            color: gold;
         }
     </style>
 </head>
 
 <body>
+    <h1 align="center" class="h1-color mt-2">Work of tracker</h1>
     <div class="container">
         <div class="row row-width mt-3">
             <form class="mb-2" align="center" action="result.php" method="POST">
+                <div class="mb-3">
+                    <label class="color" for="name" class="form-label">Name</label>
+                    <input type="text" class="form-control" name="name" id="name" placeholder="Name">
+                </div>
                 <div class="mb-3">
                     <label class="color" for="arrived_at" class="form-label">Arrived at</label>
                     <input type="datetime-local" class="form-control" name="arrived_at" id="arrived_at" aria-describedby="emailHelp">
@@ -42,6 +64,33 @@
                 </div>
                 <button type="submit" name="sub" class="btn btn-primary">Submit</button>
             </form>
+        </div>
+        <h1 align="center" class="text-warning mt-3">Working hour: <?= WORK_TIME ?></h1>
+        <div class="row mt-5">
+            <table class="table table-info table-striped">
+                <thead class="table-secondary">
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Arrived at</th>
+                        <th scope="col">Leaved at</th>
+                        <th scope="col">Required of</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    foreach ($result as $value) { ?>
+                        <tr>
+                            <th scope="row"><?= $value['id'] ?></th>
+                            <td><?= $value['name'] ?></td>
+                            <td><?= $value['arrived_at'] ?></td>
+                            <td><?= $value['leaved_at'] ?></td>
+                            <td><?= date("H:i", $value['required_of']) ?></td>
+                        </tr>
+                    <?php }
+                    ?>
+                </tbody>
+            </table>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--- result.php
+++ result.php
@@ -1,53 +1,34 @@
 <?php
 $conn = new PDO("mysql:host=localhost;dbname=work_of_tracker", 'root', 'My$par0l');
-?>
-<!doctype html>
-<html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-</head>
+const WORK_TIME = 8;
 
-<body>
-    <div class="container">
-        <div class="row mt-3">
-            <?php
-            define('WORK_TIME', 8);
 
-            if (isset($_POST['sub'])) {
-                $start = new DateTime($_POST['arrived_at']);
-                $end = new DateTime($_POST['leaved_at']);
-                $difference = $start->diff($end);
+if (isset($_POST['sub'])) {
+    $start = new DateTime($_POST['arrived_at']);
+    $end = new DateTime($_POST['leaved_at']);
+    $difference = $start->diff($end);
 
-                $arrived_at = $_POST['arrived_at'];
-                $leaved_at = $_POST['leaved_at'];
+    $hour = $difference->h;
+    $minut = $difference->i;
+    $secund = $difference->s;
 
-                $query = "INSERT INTO work_time(arrived_at, leaved_at) VALUES(:arrived_at, :leaved_at)";
+    $total = ((WORK_TIME * 3600) - ($hour * 3600) + ($minut * 60));
 
-                $stmt = $conn->prepare($query);
+    $name = $_POST['name'];
+    $arrived_at = $start->format("d.m.Y H:i");
+    $leaved_at = $end->format("d.m.Y H:i");
 
-                $stmt->bindParam(':arrived_at', $arrived_at);
-                $stmt->bindParam(':leaved_at', $leaved_at);
-                
-                $stmt->execute();
-            ?>
-                <h1 align="center">Arrived at: <?= $_POST['arrived_at'] ?></h1>
-                <h1 align="center">Leaved at: <?= $_POST['leaved_at'] ?></h1>
-                <h1 align="center">Working time: <?= WORK_TIME ?></h1>
-            <?php
-                echo "<h1 align='center'>" . "Soat: " . $difference->h . "</h1>";
-                echo "<h1 align='center'>" . "Minut: " . $difference->i . "</h1>";
-            }
+    $query = "INSERT INTO work_time(name, arrived_at, leaved_at, required_of) VALUES(:name, :arrived_at, :leaved_at, :required_of)";
 
-            ?>
-            <a href="index.php" class="btn btn-primary">Ortga qaytish</a>
-        </div>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
+    $stmt = $conn->prepare($query);
 
-</html>
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':arrived_at', $arrived_at);
+    $stmt->bindParam(':leaved_at', $leaved_at);
+    $stmt->bindParam(':required_of', $total);
+
+    $stmt->execute();
+
+    header("location: index.php");
+}
